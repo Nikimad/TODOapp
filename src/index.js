666e48d17ce86cecdc8f3f0b8b3086b093106c38
Nikimad <PybePoiD@gmail.com>
2022-12-03 19:53:11 +0300

    delete view demo, and update index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import TODOview from "./todoApp/view/view";
-import TODOapp from "./todoApp/app";
-
-const root = document.querySelector('.root');
-
-const todo = new TODOapp(() => {}, TODOview, () => {});
-
-todo.init(root);
